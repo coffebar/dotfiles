1fa7805450156412264db845cd43c15a946181df
salta <salta@e.mail>
2021-07-29 10:28:12 +0300

    move to 1 monitor

--- polybar.start.sh
+++ polybar.start.sh
@@ -8,6 +8,9 @@ PRIMARY=$(polybar -m | grep primary | awk -F: '{print $1}')
 
 MONITOR=$PRIMARY polybar --reload $BAR_NAME &
 
+# for single monitor setup
+exit 0
+
 sleep 3
 
 for i in $(polybar -m | grep -v primary | awk -F: '{print $1}'); do
