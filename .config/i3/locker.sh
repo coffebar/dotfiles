ebcf662303480eb472d716ea1ef582fbfd3d8cb9
salta <salta@e.mail>
2021-08-30 13:28:50 +0300

    xsecurelock

--- .config/i3/locker.sh
+++ .config/i3/locker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # lock screen with i3lock
-i3lock -c000000 --show-failed-attempts && sleep 1
-
+#### i3lock -c000000 --show-failed-attempts && sleep 1
+XSECURELOCK_SHOW_USERNAME=0 XSECURELOCK_SHOW_HOSTNAME=0 XSECURELOCK_SHOW_DATETIME=1 XSECURELOCK_PASSWORD_PROMPT=kaomoji xsecurelock
