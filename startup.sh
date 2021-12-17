53e4d7f300b8978ecc8c862fd845ed238bbd9f6e
salta <salta@e.mail>
2021-12-17 11:47:49 +0200

    wip

--- startup.sh
+++ startup.sh
@@ -56,11 +56,9 @@ fi
 
 # Telegram messenger
 telegram-desktop &
-# Dropbox - sync files
-# /usr/bin/dropbox start -i & # paused
 
 # Start KeePassXC with unlocking password # see https://github.com/keepassxreboot/keepassxc/issues/1267
-/bin/bash -c "secret-tool lookup 'keepass' 'default' | keepassxc --pw-stdin $HOME/Dropbox/lastpass.kdbx" &
+/bin/bash -c "secret-tool lookup 'keepass' 'default' | keepassxc --pw-stdin $HOME/Sync/Work/lastpass.kdbx" &
 # remove "urgency" from KeePassXC on startup
 /bin/bash -c 'sleep 5 && wmctrl -r KeePassXC -b remove,demands_attention' &
 
--- toggle_headphones.sh
+++ toggle_headphones.sh
@@ -9,11 +9,11 @@ OTHER_SINK_NAME=$(echo $OTHER_SINK | awk '{print $2}')
 echo "$OTHER_SINK_ID $OTHER_SINK_NAME"
 
 IS_HDMI=$(echo "$OTHER_SINK_NAME" | grep hdmi)
-if [[ $IS_HDMI ]]; then
-  pacmd set-sink-volume $OTHER_SINK_ID 50000
-else
-  pacmd set-sink-volume $OTHER_SINK_ID 30000
-fi
+#if [[ $IS_HDMI ]]; then
+#  pacmd set-sink-volume $OTHER_SINK_ID 50000
+#else
+#  pacmd set-sink-volume $OTHER_SINK_ID 30000
+#fi
 
 pacmd set-default-sink $OTHER_SINK_ID
 
