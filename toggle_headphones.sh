4e7c82d18619703b5affe220287e3a61b1b8cfd9
devse <mukyta2@gmail.com>
2022-05-15 11:19:28 +0300

    switch to pipewire

--- toggle_headphones.sh
+++ toggle_headphones.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # switch between first 2 audio output devices
 
-DEFAULT_SINK_NAME=$(pacmd info | grep -P '^Default sink name: (.*)$' | awk '{print $4}')
+DEFAULT_SINK_NAME=$(pactl info | grep -P '^Default Sink: (.*)$' | awk '{print $3}')
 OTHER_SINK=$(pactl list short sinks | grep --invert-match "$DEFAULT_SINK_NAME" | head -1)
 OTHER_SINK_ID=$(echo $OTHER_SINK | awk '{print $1}')
 OTHER_SINK_NAME=$(echo $OTHER_SINK | awk '{print $2}')
@@ -15,7 +15,7 @@ IS_HDMI=$(echo "$OTHER_SINK_NAME" | grep hdmi)
 #  pacmd set-sink-volume $OTHER_SINK_ID 30000
 #fi
 
-pacmd set-default-sink $OTHER_SINK_ID
+pactl set-default-sink $OTHER_SINK_ID
 
 notify-send "Audio Device" "$OTHER_SINK_NAME" --expire-time=1500
 
