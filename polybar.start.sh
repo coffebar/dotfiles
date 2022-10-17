53ac9857d583b51e7d91e8f55f20027fff48bdc1
salta <salta@e.mail>
2022-10-17 23:54:47 +0300

    minor changes

--- polybar.start.sh
+++ polybar.start.sh
@@ -5,6 +5,11 @@
 killall polybar
 
 PROF=$(autorandr --current)
+if [ -z $PROF ] && ! [ -z $1 ]; then
+	# try harder to find profile if script called with any arg 
+    PROF=$(autorandr | grep '(detected)')
+	echo $1
+fi
 if [ -z $PROF ]; then
     exit 0
 fi
