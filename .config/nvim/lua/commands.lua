58efed5342b92599588ec0921bcb9a7782c39149
salta <salta@e.mail>
2022-10-30 14:27:42 +0200

    optimize for large files

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -7,3 +7,10 @@ vim.api.nvim_create_autocmd('BufReadPost', {
 	desc = 'Return to last edit position when opening files',
 	command = 'if line("\'\\"") > 1 && line("\'\\"") <= line("$") | exe "normal! g\'\\"" | endif'
 })
+
+
+vim.api.nvim_create_autocmd('BufReadPre', {
+	group = augroup,
+	desc = 'Disable filetype for large files (>50MB)',
+	command = 'let f=expand("<afile>") | if getfsize(f) > 1024*1024*50 | set eventignore+=FileType | else | set eventignore-=FileType | endif'
+})
