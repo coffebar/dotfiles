0c6d57507c3b879b0c27a8a2936a69b0f54cc82f
salta <salta@e.mail>
2022-11-02 23:05:45 +0200

    improved script

--- movies.sh
+++ movies.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
-SELECTED=$(find ~/Downloads/ -type f \
+START_PATH=$HOME/Downloads
+MOVIES=$(find "$START_PATH" -type f \
 	\( -name '*.mp4' -o -name '*.mkv' \) \
 	-not -path '*/Telegram Desktop/*' \
+	-printf '%P|')
+SELECTED=$(echo "$MOVIES" \
 	| rofi -i -font "Droid Sans 16" \
 	-theme Arc-Dark -dmenu \
-	-dpi $QT_FONT_DPI)
-
-[ -z "$SELECTED" ] || [ -e "$SELECTED" ] && mpv "$SELECTED" 
+	-dpi $QT_FONT_DPI -sep '|')
+MOVIE="$START_PATH/$SELECTED" 
+[ -f "$MOVIE" ] && mpv "$MOVIE"
