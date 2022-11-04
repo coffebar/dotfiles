fb7cb076188f84b494bcb7d17bc89aecd615eaa6
salta <salta@e.mail>
2022-11-04 16:32:12 +0200

    change top bar for nvim

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -20,24 +20,7 @@ require('lualine').setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {{
-      'filename',
-      file_status = true,      -- Displays file status (readonly status, modified status)
-      newfile_status = false,  -- Display new file status (new file means no write after created)
-      path = 3,                -- 0: Just the filename
-                               -- 1: Relative path
-                               -- 2: Absolute path
-                               -- 3: Absolute path, with tilde as the home directory
-
-      shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
-                               -- for other components. (terrible name, any suggestions?)
-      symbols = {
-        modified = '[+]',      -- Text to show when the file is modified.
-        readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
-        unnamed = '[No Name]', -- Text to show for unnamed buffers.
-        newfile = '[New]',     -- Text to show for new created file before first writting
-      }
-    }},
+    lualine_c = {'filename'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
@@ -50,14 +33,7 @@ require('lualine').setup {
     lualine_y = {},
     lualine_z = {}
   },
-  -- tabline = {
-  --   lualine_a = {'buffers'},
-  --   lualine_b = {'branch'},
-  --   lualine_c = {},
-  --   lualine_x = {},
-  --   lualine_y = {},
-  --   lualine_z = {'tabs'}
-  -- },
+  tabline = {},
   winbar = {},
   inactive_winbar = {},
   extensions = {}
