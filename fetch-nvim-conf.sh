dcf38183c0df87775e19fe83d9370fa84f287e3d
Saltaformajo <i8ehkvien@mozmail.com>
2022-11-06 16:18:55 +0200

    tested on ubuntu

--- fetch-nvim-conf.sh
+++ fetch-nvim-conf.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
+if cat /etc/os-release | grep Ubuntu ; then
+	echo "Ubuntu is not supported for now. Reason: snap version has broken tree-sitter, apt version is too old for tree-sitter"
+	exit -1
+	sudo apt install -y git rsync npm ripgrep neovim
+	sudo snap install go --classic
+	git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+ 	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+	~/.fzf/install
+else
+	echo "Using yay to install required packages"
+	yay -Sy --needed neovim go npm ltex-ls-bin rust-analyzer phpactor \
+		lua-language-server nvim-packer-git fd ripgrep fzf
+fi
+
 mkdir -p ~/.config/nvim
 
-git clone --depth=1 git@github.com:coffebar/dotfiles.git /tmp/dotfiles_tmp_nvim
+git clone --depth=1 https://github.com/coffebar/dotfiles.git /tmp/dotfiles_tmp_nvim
 rsync -rv --delete /tmp/dotfiles_tmp_nvim/.config/nvim/ ~/.config/nvim/
 rm -rf /tmp/dotfiles_tmp_nvim/
-
-yay -Sy --needed neovim go npm ltex-ls-bin rust-analyzer phpactor lua-language-server nvim-packer-git fd ripgrep
-# todo: build a list of packages for ubuntu repo
 
 npm config set prefix ~/.node_modules
 
