fd324ff3b56198628441ca7d328634000cfbfe0c
devse <corp@m.d>
2022-11-08 17:44:58 +0200

    Open images in external viewer-editor

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -8,6 +8,32 @@ vim.api.nvim_create_autocmd('BufReadPost', {
 	command = 'if line("\'\\"") > 1 && line("\'\\"") <= line("$") | exe "normal! g\'\\"" | endif'
 })
 
+vim.api.nvim_create_autocmd('BufReadPost', {
+	group = augroup,
+	pattern = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.svg", },
+	desc = 'Open images in external viewer-editor',
+	callback = function()
+		local image_viewers = {
+			-- first found program will be used to open image
+			"pix", -- https://github.com/linuxmint/pix
+			"feh",
+		}
+		-- search for installed program
+		local program
+		for _, p in ipairs(image_viewers) do
+			if vim.fn.executable(p) == 1 then
+				program = p
+				break
+			end
+		end
+		if program then
+			vim.api.nvim_command('AsyncRun -silent ' .. program .. ' %:p &')
+			-- image buffer is not useful in binary representation.
+			-- Will close buffer without closing a window
+			vim.api.nvim_command('bp | sp | bn | bd')
+		end
+	end
+})
 
 vim.api.nvim_create_autocmd('BufReadPre', {
 	group = augroup,
