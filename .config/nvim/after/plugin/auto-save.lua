03fe86261265bf3af26644227e4455adc530a332
devse <corp@m.d>
2022-11-11 12:54:24 +0200

    don't auto-save gitcommit message

--- .config/nvim/after/plugin/auto-save.lua
+++ .config/nvim/after/plugin/auto-save.lua
@@ -15,7 +15,9 @@ require("auto-save").setup({
 		local fn = vim.fn
 		local utils = require("auto-save.utils.data")
 
-		if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+		if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {
+			"gitcommit",
+		}) then
 			return true -- met condition(s), can save
 		end
 		return false -- can't save
