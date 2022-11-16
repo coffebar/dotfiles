7bd29e1046637a2d673c2d50e8c09eca61b842c9
devse <corp@m.d>
2022-11-16 13:51:47 +0200

    exclude html from auto-save Reason: bad ux with autopairs+autoformatting

--- .config/nvim/after/plugin/auto-save.lua
+++ .config/nvim/after/plugin/auto-save.lua
@@ -14,10 +14,11 @@ require("auto-save").setup({
 	condition = function(buf)
 		local fn = vim.fn
 		local utils = require("auto-save.utils.data")
-
-		if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {
+		local exclude = {
 			"gitcommit",
-		}) then
+			"html",
+		}
+		if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), exclude) then
 			return true -- met condition(s), can save
 		end
 		return false -- can't save
