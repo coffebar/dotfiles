1f69eafb5a3b8f194b2fcfb130e78f234244080d
devse <corp@m.d>
2022-11-16 13:41:17 +0200

    add ignore dirs for project plugin

--- .config/nvim/plugin/packer.lua
+++ .config/nvim/plugin/packer.lua
@@ -52,6 +52,12 @@ return require("packer").startup(function(use)
 			require("project_nvim").setup({
 				session_autoload = true,
 				silent_chdir = false,
+				exclude_dirs = {
+					"~/.local/*",
+					"~/.cache/*",
+					"~/.cargo/*",
+					"~/.node_modules/*",
+				},
 			})
 		end,
 	})
