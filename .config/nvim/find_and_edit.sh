c3e235053aeb3b01c26ffc7978c6de9ed6e19405
devse <corp@m.d>
2022-11-17 21:35:19 +0200

    formatting

--- .config/nvim/find_and_edit.sh
+++ .config/nvim/find_and_edit.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# run (neo)vim to search and open a single file
-GIT_DIR=$HOME/dotfiles GIT_WORK_TREE=$HOME $EDITOR -c 'bd! | SearchInHome' _
+# run [Neo]vim to search and open some file
+GIT_DIR=$HOME/dotfiles GIT_WORK_TREE=$HOME \
+	$EDITOR -c 'bd! | SearchInHome' _
