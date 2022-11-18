5ab73025919e7e28adf06916d5b07d24cfb5fea5
devse <corp@m.d>
2022-11-18 23:26:42 +0200

    playing with colorscheme

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -2,13 +2,13 @@ local onedark = require("onedark")
 local g = vim.g
 
 onedark.setup({
-	style = "warmer",
+	style = "warm",
 	transparent = false, -- Show/hide background
 	term_colors = false, -- Change terminal color as per the selected theme style
 
 	-- toggle theme style ---
-	toggle_style_key = "<leader>co", -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-	toggle_style_list = { "darker", "deep", "warmer" }, -- List of styles to toggle between
+	-- toggle_style_key = "<leader>co", -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+	-- toggle_style_list = { "darker", "deep", "warmer" }, -- List of styles to toggle between
 	lualine = {
 		transparent = false, -- lualine center bar transparency
 	},
@@ -16,6 +16,13 @@ onedark.setup({
 		grey = "#8b8b8b",
 		fg = "#bbbbbb",
 		bg0 = "#2b2e37",
+	},
+	code_style = {
+		comments = "none",
+		keywords = "none",
+		functions = "none",
+		strings = "none",
+		variables = "none",
 	},
 })
 onedark.load()
