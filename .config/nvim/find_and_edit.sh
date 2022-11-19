8b5337a6c12636b2725275b89a866c8d7128dcb7
Saltaformajo <i8ehkvien@mozmail.com>
2022-11-19 16:09:08 +0200

    check if dotfiles not cloned or editor is not set to (n)vim

--- .config/nvim/find_and_edit.sh
+++ .config/nvim/find_and_edit.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 # run [Neo]vim to search and open some file
-GIT_DIR=$HOME/dotfiles GIT_WORK_TREE=$HOME \
+if [ -z $EDITOR ] || ! [[ $EDITOR == *"vim" ]]; then
+	EDITOR=nvim
+fi
+if [ -d $HOME/dotfiles ]; then
+	GIT_DIR=$HOME/dotfiles GIT_WORK_TREE=$HOME \
+		$EDITOR -c 'bd! | SearchInHome' _
+else
 	$EDITOR -c 'bd! | SearchInHome' _
+fi
