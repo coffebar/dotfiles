0058dbea96a80c7239ac8fa14942cf592d2f076e
devse <corp@m.d>
2022-11-20 17:04:54 +0200

    formatting for autocompletion

--- .config/nvim/after/plugin/nvim-cmp.lua
+++ .config/nvim/after/plugin/nvim-cmp.lua
@@ -2,6 +2,34 @@
 local cmp = require("cmp")
 require("luasnip.loaders.from_lua").lazy_load({ paths = vim.fn.stdpath("config") .. "/snippets/" })
 
+local kind_icons = {
+	Text = "",
+	Method = "",
+	Function = "",
+	Constructor = "",
+	Field = "",
+	Variable = "",
+	Class = "ﴯ",
+	Interface = "",
+	Module = "",
+	Property = "ﰠ",
+	Unit = "",
+	Value = "",
+	Enum = "",
+	Keyword = "",
+	Snippet = "",
+	Color = "",
+	File = "",
+	Reference = "",
+	Folder = "",
+	EnumMember = "",
+	Constant = "",
+	Struct = "",
+	Event = "",
+	Operator = "",
+	TypeParameter = "",
+}
+
 cmp.setup({
 	snippet = {
 		-- REQUIRED - you must specify a snippet engine
@@ -36,6 +64,29 @@ cmp.setup({
 			keyword_length = 3,
 		},
 	}),
+	formatting = {
+		format = function(entry, vim_item)
+			-- Kind icons
+			vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+			-- Source
+			vim_item.menu = ({
+				luasnip = "[LuaSnip]",
+				nvim_lua = "[Lua]",
+				latex_symbols = "[LaTeX]",
+				gh_issues = "[Issues]",
+				buffer = "[Buf]",
+				nvim_lsp = "[LSP]",
+				path = "[Path]",
+				tmux = "[Tmux]",
+				look = "[Look]",
+				rg = "[RG]",
+				crates = "[Crates]",
+				orgmode = "[ORG]",
+				dap = "[DAP]",
+			})[entry.source.name]
+			return vim_item
+		end,
+	},
 })
 
 -- Set configuration for specific filetype.
