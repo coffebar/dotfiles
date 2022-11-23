69230a6ffbb0a4fe55cfd2d8c2351ab166fc3b6a
devse <corp@m.d>
2022-11-23 14:58:39 +0200

    Snippets for javascript

---
+++ .config/nvim/snippets/javascript.lua
@@ -0,0 +1,31 @@
+local ls = require("luasnip")
+-- local s = ls.snippet
+-- local sn = ls.snippet_node
+-- local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+-- local f = ls.function_node
+-- local c = ls.choice_node
+-- local d = ls.dynamic_node
+-- local r = ls.restore_node
+-- local events = require("luasnip.util.events")
+-- local ai = require("luasnip.nodes.absolute_indexer")
+-- local extras = require("luasnip.extras")
+-- local l = extras.lambda
+-- local rep = extras.rep
+-- local p = extras.partial
+-- local m = extras.match
+-- local n = extras.nonempty
+-- local dl = extras.dynamic_lambda
+-- local fmt = require("luasnip.extras.fmt").fmt
+-- local fmta = require("luasnip.extras.fmt").fmta
+-- local conds = require("luasnip.extras.expand_conditions")
+-- local postfix = require("luasnip.extras.postfix").postfix
+-- local types = require("luasnip.util.types")
+-- local parse = require("luasnip.util.parser").parse_snippet
+
+-- Snippets for JavaScript
+return {
+	ls.s({ trig = "log", name = "console.log();" }, { t("console.log("), i(1), t(");") }),
+	ls.s({ trig = "warn", name = "console.warn();" }, { t("console.warn("), i(1), t(");") }),
+}
