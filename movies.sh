8a189cab25c75bf34f956360a7e08d6ed2119131
salta <salta@example.com>
2022-11-27 20:01:40 +0200

    Merge 2 versions of movies.sh

--- movies.sh
+++ movies.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 START_PATH=$HOME/Downloads
-MOVIES=$(find "$START_PATH" -type f \
+SELECTED=$(find "$START_PATH" -type f \
 	\( -name '*.mp4' -o -name '*.mkv' \) \
 	-not -path '*/Telegram Desktop/*' \
-	-printf '%P|')
-SELECTED=$(echo "$MOVIES" \
+	-size +100M \
+	-printf '%P|' \
 	| rofi -i -font "Droid Sans 16" \
 	-theme Arc-Dark -dmenu \
 	-dpi $QT_FONT_DPI -sep '|')
 MOVIE="$START_PATH/$SELECTED" 
-[ -f "$MOVIE" ] && mpv "$MOVIE"
+[ -f "$MOVIE" ] && mpv \
+	-alang="ukr,uk,eng,en" -slang="eng,en,ukr,uk" \
+	--fullscreen "$MOVIE"
