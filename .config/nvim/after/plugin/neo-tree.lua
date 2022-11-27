4e408ddb39f868bc7e4b47a5de51d62e4a4cfd07
devse <corp@m.d>
2022-11-27 17:08:32 +0200

    conf for neotree

---
+++ .config/nvim/after/plugin/neo-tree.lua
@@ -0,0 +1,68 @@
+require("neo-tree").setup({
+	window = {
+		position = "left",
+		width = 40,
+		mapping_options = {
+			noremap = true,
+			nowait = true,
+		},
+		mappings = {
+			["<space>"] = {
+				"toggle_node",
+				nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
+			},
+			["<2-LeftMouse>"] = "open",
+			["<cr>"] = "open",
+			["<esc>"] = "revert_preview",
+			["P"] = { "toggle_preview", config = { use_float = true } },
+			["S"] = "open_split",
+			["s"] = "open_vsplit",
+			-- ["S"] = "split_with_window_picker",
+			-- ["s"] = "vsplit_with_window_picker",
+			["t"] = "open_tabnew",
+			-- ["<cr>"] = "open_drop",
+			-- ["t"] = "open_tab_drop",
+			["w"] = "open_with_window_picker",
+			--["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
+			["C"] = "close_node",
+			["z"] = "close_all_nodes",
+			--["Z"] = "expand_all_nodes",
+			["a"] = {
+				"add",
+				-- some commands may take optional config options, see `:h neo-tree-mappings` for details
+				config = {
+					show_path = "none", -- "none", "relative", "absolute"
+				},
+			},
+			["A"] = "add_directory", -- also accepts the optional config.show_path option like "add".
+			["d"] = "delete",
+			["r"] = "rename",
+			["y"] = "copy_to_clipboard",
+			["x"] = "cut_to_clipboard",
+			["p"] = "paste_from_clipboard",
+			["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
+			-- ["c"] = {
+			--  "copy",
+			--  config = {
+			--    show_path = "none" -- "none", "relative", "absolute"
+			--  }
+			--}
+			["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
+			["q"] = "close_window",
+			["R"] = "refresh",
+			["?"] = "show_help",
+			["<"] = "prev_source",
+			[">"] = "next_source",
+
+			["Y"] = function(state)
+				local node = state.tree:get_node()
+				local content = node.path
+				-- relative
+				-- local content = node.path:gsub(state.path, ""):sub(2)
+				vim.fn.setreg('"', content)
+				vim.fn.setreg("1", content)
+				vim.fn.setreg("+", content)
+			end,
+		},
+	},
+})
