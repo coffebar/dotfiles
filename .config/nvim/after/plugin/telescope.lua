174553a27a9b626487bf9813583422f230adec0c
salta <salta@example.com>
2022-11-27 18:36:46 +0200

    set gitignore as arg to rg

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -18,7 +18,16 @@ telescope.setup({
 	pickers = {
 		find_files = {
 			-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
-			find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*" },
+			find_command = {
+				"rg",
+				"--files",
+				"--hidden",
+				"--glob",
+				"!.git/*",
+				"--ignore-file",
+				"~/.gitignore",
+			},
+			--
 		},
 	},
 	extensions = {
