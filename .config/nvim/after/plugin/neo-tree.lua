8bbbb49a2cd9f1b40af9fb034bf936824a04f8c8
salta <salta@example.com>
2022-11-30 20:14:19 +0200

    Neo-tree options changed to show dotfiles

--- .config/nvim/after/plugin/neo-tree.lua
+++ .config/nvim/after/plugin/neo-tree.lua
@@ -1,4 +1,31 @@
 require("neo-tree").setup({
+	filesystem = {
+		filtered_items = {
+			visible = false, -- when true, they will just be displayed differently than normal items
+			hide_dotfiles = false,
+			hide_gitignored = true,
+			hide_hidden = true, -- only works on Windows for hidden files/directories
+			hide_by_name = {
+				".DS_Store",
+				"thumbs.db",
+				--"node_modules",
+			},
+			hide_by_pattern = {
+				--"*.meta",
+				--"*/src/*/tsconfig.json",
+			},
+			always_show = { -- remains visible even if other settings would normally hide it
+				--".gitignored",
+			},
+			never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+				--".DS_Store",
+				--"thumbs.db",
+			},
+			never_show_by_pattern = { -- uses glob style patterns
+				--".null-ls_*",
+			},
+		},
+	},
 	window = {
 		position = "left",
 		width = 40,
