b9abe908febf61f877ce5c4090755a78926c55e6
salta <salta@example.com>
2022-12-01 21:29:40 +0200

    use new i3's "include" feature

--- .config/i3/join.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-COMMON_PART="$HOME/.config/i3/i3config.part"
-cat $COMMON_PART "$HOME/.config/potato/i3config.part" > "$HOME/.config/potato/i3config"
-cat $COMMON_PART "$HOME/.config/kindle/i3config.part" > "$HOME/.config/kindle/i3config"
