e7e8df0f5fb84522cc5227d0bf1b48af655ca8d3
coffeebar <corp@dev.null>
2023-02-04 22:32:02 +0200

    wip

--- .config/crab/polybar.sh
+++ .config/crab/polybar.sh
@@ -2,17 +2,54 @@
 # start polybar for each monitor
 # starting from the primary one
 
-killall polybar && sleep 1
+setup_hdmi1_2k() {
+	# add monitor HDMI1 with "2560x1440_60.00" mode
+	xrandr --newmode "2560x1440_60.00"  312.25  2560 2752 3024 3488  1440 1443 1448 1493 -hsync +vsync
+	xrandr --addmode HDMI1 "2560x1440_60.00"
+	xrandr --output HDMI1 --mode "2560x1440_60.00" --pos 0x0 \
+		--output eDP1 --primary --mode 1920x1200 --pos 320x1440
+}
 
-BAR_NAME=default
+setup_monitors() {
+	local NEED_HDMI=0
+	local NEED_NO_HDMI=0
 
-PRIMARY=$(polybar -m | grep primary | awk -F: '{print $1}')
+	if xrandr | grep 'HDMI1 connected' ; then
+		setup_hdmi1_2k
+		NEED_HDMI=1
+	else
+		# disable HDMI1, setup correct resolution and position
+		xrandr --output HDMI1 --off \
+			--output eDP1 --pos 0x0 --primary --mode 1920x1200 --pos 0x0 ;
+		NEED_NO_HDMI=1
+	fi
 
-POLYBAR_CONF="$HOME/.config/$XDG_SESSION_OPT/polybar.ini"
-MONITOR=$PRIMARY polybar -c "$POLYBAR_CONF" --reload $BAR_NAME &
+	# wait until xrandr finished job
+	if [ $NEED_HDMI -eq 1 ] ; then
+		until xrandr | grep 'HDMI1 connected'; do sleep 1; done; 
+		dunstify -t 2000 "HDMI1 connected"
+	fi
+	if [ $NEED_NO_HDMI -eq 1 ] ; then
+		until xrandr | grep -v 'HDMI1 connected'; do sleep 1; done; 
+	fi
+}
 
-sleep 3
+start_polybar() {
+	local BAR_NAME=default
+	local PRIMARY
+	local MONITOR
+	local POLYBAR_CONF
+	PRIMARY=$(polybar -m | grep primary | awk -F: '{print $1}')
+	POLYBAR_CONF="$HOME/.config/$XDG_SESSION_OPT/polybar.ini"
+	MONITOR=$PRIMARY polybar -c "$POLYBAR_CONF" --reload $BAR_NAME &
+	sleep 3 # wait for tray
+	for i in $(polybar -m | grep -v primary | awk -F: '{print $1}'); do
+		MONITOR=$i polybar -c "$POLYBAR_CONF" --reload $BAR_NAME &
+	done
+}
 
-for i in $(polybar -m | grep -v primary | awk -F: '{print $1}'); do
-    MONITOR=$i polybar -c "$POLYBAR_CONF" --reload $BAR_NAME &
-done
+killall polybar
+setup_monitors
+# wallpaper
+feh --bg-scale ~/Pictures/wallpapers/bridge.jpg
+start_polybar
--- .zprofile
+++ .zprofile
@@ -1,13 +1,18 @@
 if [ -z "$DISPLAY" ] && [ -n "$XDG_VTNR" ] && [ "$XDG_VTNR" -eq 1 ]; then
 	if [ -f /usr/bin/Hyprland ]; then
-		echo 'Hyprland?'
-    read -r X
-		if [[ "$X" == 'y' ]]; then
-			XDG_SESSION_TYPE="wayland"
-			if [ -f $HOME/.config/crab/.profile ]; then
-					source $HOME/.config/crab/.profile
+		echo 'Continue to Hyprland?'
+		# you have 3 seconds to press any key except Return to load i3wm
+		# otherwise Hyprland will be loaded
+    read -t 3 -r -s -k 1 X
+		if [ -z "$X" ]; then
+			export XDG_SESSION_OPT="crab"
+			export XDG_SESSION_TYPE="wayland"
+			# Log WLR errors and logs to the hyprland log
+			export HYPRLAND_LOG_WLR=1
+			if [ -f "$HOME/.config/$XDG_SESSION_OPT/.profile" ]; then
+					source "$HOME/.config/$XDG_SESSION_OPT/.profile"
 			fi
-			exec /usr/bin/Hyprland 
+			exec /usr/bin/Hyprland -c "$HOME/.config/$XDG_SESSION_OPT/hyprland.conf"
 	  else
 			exec startx > /dev/null
 		fi
