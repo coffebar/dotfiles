1deeaedf81fee08938e6c7b28e5a665ae45c2ede
coffeebar <corp@dev.null>
2023-02-08 17:52:01 +0200

    tool for laravel

---
+++ laravel-open-project.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Open laravel dev env on Hyprland wayland compositor:
+# start editor, browser, VM, show logs, run queue, build frontend, open terminal
+# class needed to pin windows on workspaces
+
+# first argument is project's dir
+PROJECT=$1
+# second is browser's profile dir, related to default config dir
+CHROME_PROFILE=$2
+
+ter (){
+	$TERMINAL -o font.size=14 --working-directory=$PROJECT $@
+}
+
+workspace (){
+	hyprctl dispatch workspace "$1" &
+}
+
+# run VM
+ter --class='wp-5' --command ./vendor/bin/sail up &
+
+# open editor
+ter --class='wp-3' --command nvim &
+
+workspace 5
+sleep 3
+
+# run queue jobs
+ter --class='wp-5' --command ./vendor/bin/sail artisan schedule:run &
+# auto build frontend
+ter --class='wp-5' --command ./vendor/bin/sail npm run dev &
+
+# switch workspaces
+workspace 4 # first display
+workspace 3 # second display
+
+
+# tail logs
+ter --class='wp-4' --command tail -f storage/logs/laravel.log &
+# run chrome
+google-chrome-stable --ozone-platform=wayland \
+	--profile-directory="$CHROME_PROFILE" &
+sleep 1
+# open terminal
+ter --class='wp-4' &
+
+disown
