6e33d0f12e5cdf06e0ffb5f4e6ccd420fcb9603f
coffeebar <corp@dev.null>
2023-02-15 19:11:09 +0200

    movies.sh for wayland

--- movies.sh
+++ movies.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 START_PATH=$HOME/Downloads
-SELECTED=$(find "$START_PATH" -type f \
+
+if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
+	RG_OUTPUT=$(rg --files --max-depth=3 -N \
+		--iglob '*.mp4' --iglob '*.mkv' --iglob '*.avi' \
+		--glob '!*Telegram Desktop*' \
+		"$START_PATH")
+	SELECTED=$(echo "$RG_OUTPUT" | sed "s+$START_PATH/++g" | wofi --show dmenu)
+else
+	SELECTED=$(find "$START_PATH" -type f \
 	\( -name '*.mp4' -o -name '*.mkv' -o -name '*.avi' \) \
 	-not -path '*/Telegram Desktop/*' \
 	-size +100M \
@@ -8,6 +16,8 @@ SELECTED=$(find "$START_PATH" -type f \
 	| rofi -i -font "Droid Sans 16" \
 	-theme Arc-Dark -dmenu \
 	-dpi $QT_FONT_DPI -sep '|')
+fi
+
 MOVIE="$START_PATH/$SELECTED" 
 if [ -f "$MOVIE" ]; then
 	killall mpv
