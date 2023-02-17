014095ae4b6a0066219ee1b8c088d1df7c8c7061
coffeebar <corp@dev.null>
2023-02-17 14:41:17 +0200

    change sort order to make it work on current hardware

--- toggle_headphones.sh
+++ toggle_headphones.sh
@@ -2,7 +2,7 @@
 # switch between first 2 audio output devices
 
 DEFAULT_SINK_NAME=$(pactl get-default-sink)
-OTHER_SINK=$(pactl list short sinks | grep --invert-match "$DEFAULT_SINK_NAME" | head -1)
+OTHER_SINK=$(pactl list short sinks | grep --invert-match "$DEFAULT_SINK_NAME" | tail -1)
 OTHER_SINK_ID=$(echo $OTHER_SINK | awk '{print $1}')
 OTHER_SINK_NAME=$(echo $OTHER_SINK | awk '{print $2}')
 
