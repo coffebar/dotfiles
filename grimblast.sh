a15a8525c76d27103cbaf4f1853f9a9599042ab3
coffeebar <corp@dev.null>
2023-02-23 16:29:38 +0200

    improve screen-shooting

--- grimblast.sh
+++ grimblast.sh
@@ -1,6 +1,9 @@
 #!/bin/zsh
-
+TARGET="area"
+if [[ "$1" == "screen" ]]; then
+	TARGET=$1
+fi
 FNAME="/tmp/ksnip/$(date +'%s')/screenshot.png"
 mkdir -p $(dirname "$FNAME")
-grimblast --notify copysave area "$FNAME"
+grimblast --notify copysave $TARGET "$FNAME"
 ksnip -e "$FNAME" &
