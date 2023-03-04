5f32949079f80ebd1c4930a1a6540e0d41846c6d
coffeebar <i8ehkvien@mozmail.com>
2023-03-04 16:22:00 +0200

    replace IF with custom script call

--- .config/nvim/project-run.sh
+++ .config/nvim/project-run.sh
@@ -3,11 +3,7 @@
 # looking for *.sh files in project
 SCRIPTS=$(rg --max-depth=2 --hidden --files --glob '*.sh')
 if [ -z "$SCRIPTS" ]; then exit; fi
-# give to select one
-if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
-	SELECTED=$(echo "$SCRIPTS" | wofi --show dmenu)
-else
-	SELECTED=$(echo "$SCRIPTS" | rofi -i -theme Arc-Dark -dmenu -dpi "$QT_FONT_DPI")
-fi
+# give to select one via rofi\wofi
+SELECTED=$(echo "$SCRIPTS" | ofi-menu)
 # execute that file in bash
 [ -n "$SELECTED" ] && [ -f "$SELECTED" ] && source "$SELECTED"
