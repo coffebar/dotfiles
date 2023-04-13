a0fc4c61b172aaea3c624cd5c75477036bbecc11
coffeebar <i8ehkvien@mozmail.com>
2023-04-13 13:56:23 +0300

    feat(nvim): open regular files without sessions

--- .config/nvim/after/plugin/session-manager.lua
+++ .config/nvim/after/plugin/session-manager.lua
@@ -1,4 +1,5 @@
 local installed, session_manager = pcall(require, "session_manager")
+
 if installed then
 	local function contains(table, element)
 		for _, value in pairs(table) do
@@ -9,21 +10,56 @@ if installed then
 		return false
 	end
 
+	local session_enabled = true
 	local Autoload = require("session_manager.config").AutoloadMode
 	local mode = Autoload.LastSession -- Possible values: Disabled, CurrentDir, LastSession
-	local project_root, _ = require("project_nvim.project").get_project_root()
-	if project_root ~= nil then
-		mode = Autoload.CurrentDir
-	end
 	if contains(vim.v.argv, "--") or contains(vim.v.argv, "SearchInHome") then
 		mode = Autoload.Disabled
+		session_enabled = false
+	end
+
+	require("project_nvim").setup({
+		session_autoload = session_enabled,
+		silent_chdir = false,
+		exclude_dirs = {
+			"~/.local/*",
+			"~/.cache/*",
+			"~/.cargo/*",
+			"~/.node_modules/*",
+			"~/lua",
+		},
+		ignore_lsp = {
+			"lua_ls",
+		},
+		patterns = {
+			"!>home",
+			"!=tmp",
+			".git",
+			".idea",
+			".svn",
+			"PKGBUILD",
+			"composer.json",
+			"package.json",
+			"Makefile",
+			"README.md",
+			"Cargo.toml",
+			">Work",
+			">pets",
+		},
+	})
+
+	if session_enabled then
+		local project_root, _ = require("project_nvim.project").get_project_root()
+		if project_root ~= nil then
+			mode = Autoload.CurrentDir
+		end
 	end
 
 	session_manager.setup({
 		path_replacer = "__", -- The character to which the path separator will be replaced for session files.
 		colon_replacer = "++", -- The character to which the colon symbol will be replaced for session files.
 		autoload_mode = mode, -- Define what to do when Neovim is started without arguments.
-		autosave_last_session = mode ~= Autoload.Disabled, -- Automatically save last session on exit and on session switch.
+		autosave_last_session = session_enabled, -- Automatically save last session on exit and on session switch.
 		-- I'l keep it false for smooth session switching
 		autosave_ignore_not_normal = false, -- Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed.
 		autosave_ignore_dirs = {
