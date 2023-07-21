b26fe41fc39199d0b09917fa29e277c83fdf51e9
coffeebar <i8ehkvien@mozmail.com>
2023-07-21 15:42:17 +0300

    fix(waybar): restart on crash

--- .config/hyprland/start-in-tray.sh
+++ .config/hyprland/start-in-tray.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 # start waybar and programs with tray icons after pause
-waybar &
-sleep 1
+
+waybar_loop() {
+	# restart waybar on crash (after suspend mode)
+	WAYBAR_RESTARTS=0
+	while [ $WAYBAR_RESTARTS != 6 ]; # limited to 5 restars
+	do
+		waybar
+		let WAYBAR_RESTARTS++
+		sleep 1
+	done
+}
+
+waybar_loop &
+
+sleep 1 # wait for waybar
+
 nm-applet --indicator &
 crow &
 syncthingtray --wait &
