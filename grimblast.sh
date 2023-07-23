8062044972215d2fe2c06fd260c475611f7919ba
coffeebar <i8ehkvien@mozmail.com>
2023-07-23 16:34:11 +0300

    feat(grimblast): put screenshots into single dir

--- grimblast.sh
+++ grimblast.sh
@@ -3,7 +3,17 @@ TARGET="area"
 if [[ "$1" == "screen" ]]; then
 	TARGET=$1
 fi
-FNAME="/tmp/ksnip/$(date +'%s')/screenshot.png"
+# filename available in the /tmp (without timezone leek)
+FNAME=$(python -c "import os
+num = 1
+dir = '/tmp/ksnip/screenshot-'
+while os.path.exists(dir + str(num) + '.png'):
+	num += 1
+print(dir + str(num) + '.png')")
 mkdir -p $(dirname "$FNAME")
+# put filename to the clipboard history
+wl-copy "$FNAME"
+# take screenshot and copy image data to clipboard
 grimblast --notify copysave $TARGET "$FNAME"
+# add a file to ksnip history to edit it
 ksnip -e "$FNAME" &
