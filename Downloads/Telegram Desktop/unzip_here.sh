a9ac49ac1d9d199a67d9a0e9140ee32bf18ba5eb
coffeebar <i8ehkvien@mozmail.com>
2023-09-07 17:35:37 +0300

    feat(unzip_here): use cli tools over file-roller

--- Downloads/Telegram Desktop/unzip_here.sh
+++ Downloads/Telegram Desktop/unzip_here.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
-# Script waits for archive files downloaded by Telegram 
-# and extracts them to the upper directory "Downloads"
+# Extract archive files downloaded by Telegram into "Downloads"
 
-cd "$(dirname "$0")"
+pushd "$(dirname "$0")" || exit 1
 inotifywait --format '%f' -m -e close_write -e moved_to "./" |\
 (
-while read
+while read -r
 do
-    FILE_EXT=$(echo $REPLY | grep --only-matching --ignore-case -P "\.(zip|rar|7z)$")
-    if [ "$FILE_EXT" != "" ]
-    then
-      DIR_NAME=$(basename "$REPLY" $FILE_EXT)
-      echo $DIR_NAME
-      file-roller --extract-here "$REPLY" && mv "$DIR_NAME" ../
-    fi
+	FILE_EXT=$(echo "$REPLY" | grep --only-matching --ignore-case -P "\.(zip|rar|7z)$")
+	if [ "$FILE_EXT" != "" ]
+	then
+	 DIR_NAME=$(basename "$REPLY" "$FILE_EXT")
+	 OUT_DIR="../$DIR_NAME"
+		if [ ! -d "$OUT_DIR" ]; then
+			mkdir "$OUT_DIR"
+			if [ "$FILE_EXT" == ".zip" ] || [ "$FILE_EXT" == ".ZIP" ]; then
+				unzip "$REPLY" -d "$OUT_DIR"
+			elif [ "$FILE_EXT" == ".rar" ] || [ "$FILE_EXT" == ".RAR" ]; then
+				unrar x "$REPLY" "$OUT_DIR"
+			elif [ "$FILE_EXT" == ".7z" ] || [ "$FILE_EXT" == ".7Z" ]; then
+				7z x "$REPLY" -o"$OUT_DIR"
+			fi
+		fi
+	fi
 done
 )
