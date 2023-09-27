e845638564aaca5fcad2caacb5f8b2ac120f84d5
coffeebar <i8ehkvien@mozmail.com>
2023-09-27 18:45:10 +0300

    chore: add wget to download stubs

--- fetch-nvim-conf.sh
+++ fetch-nvim-conf.sh
@@ -5,7 +5,7 @@ if grep Ubuntu /etc/os-release; then
 else
 	pacman --version || exit 1
 	# packages from arch repo
-	PKG=(autopep8 ansible-lint neovim go npm rust-analyzer lua-language-server fd ripgrep xclip rsync python-virtualenv)
+	PKG=(autopep8 ansible-lint neovim go npm rust-analyzer lua-language-server fd ripgrep xclip rsync python-virtualenv wget)
 	# chech all packages if installed
 	# to avoid asking for sudo if nothing will be installed
 	TO_INSTALL=()
