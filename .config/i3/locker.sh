76892dbe1707764769ed38cb3f5b355e7d4da4b4
coffeebar <i8ehkvien@mozmail.com>
2023-10-02 20:52:18 +0300

    fix(i3): refresh current workspace after compositing restart

--- .config/i3/locker.sh
+++ .config/i3/locker.sh
@@ -17,3 +17,7 @@ XSECURELOCK_COMPOSITE_OBSCURER=0 XSECURELOCK_SHOW_USERNAME=0 \
 	XSECURELOCK_SHOW_HOSTNAME=0 XSECURELOCK_SHOW_DATETIME=1 \
 	XSECURELOCK_PASSWORD_PROMPT=kaomoji xsecurelock \
 	&& xcompmgr -n & # restart compositing
+
+# refresh current workspace after compositing restart
+sleep 0.3
+i3-msg workspace 9 && i3-msg workspace 9
