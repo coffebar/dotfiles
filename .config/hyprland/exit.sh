df705013fa350475c2a86023ecf99f29ad7220ad
coffeebar <i8ehkvien@mozmail.com>
2023-10-03 19:16:06 +0300

    wip(hyprland): replace `exit` command with a script with logging and kill-9

---
+++ .config/hyprland/exit.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# exit hyprland with force
+
+echo "Hyprland exit" | systemd-cat -t coffebar -p info
+hyprctl dispatch exit &
+
+sleep 10
+echo "Hyprland failed to exit" | systemd-cat -t coffebar -p err
+kill -9 "$PID"
