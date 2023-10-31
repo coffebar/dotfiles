41ded54f53208517cd0e8e56c0a82e7251f17dcf
Saltaformajo <i8ehkvien@mozmail.com>
2023-10-31 09:04:19 +0200

    fix: remove nvim-cmp is not installed message

--- .config/nvim/after/plugin/nvim-cmp.lua
+++ .config/nvim/after/plugin/nvim-cmp.lua
@@ -1,7 +1,6 @@
 -- Set up nvim-cmp (completion engine plugin)
 local cmp_installed, cmp = pcall(require, "cmp")
 if not cmp_installed then
-  vim.notify("nvim-cmp isn't installed", vim.log.levels.ERROR)
   return
 end
 require("luasnip.loaders.from_lua").lazy_load({ paths = vim.fn.stdpath("config") .. "/snippets/" })
