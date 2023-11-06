6355be114bab4250a2a4b2f6157dbf4ea2ac206e
Saltaformajo <i8ehkvien@mozmail.com>
2023-11-06 18:00:58 +0200

    Revert "wip: exit.sh: increase sleep time to debug exit"
    This reverts commit bfd03d13bb689a488c1db512a9690c80ca172803.

--- .config/hyprland/exit.sh
+++ .config/hyprland/exit.sh
@@ -4,6 +4,6 @@
 echo "Hyprland exit" | systemd-cat -t coffebar -p info
 hyprctl dispatch exit &
 
-sleep 360
+sleep 10
 echo "Hyprland failed to exit" | systemd-cat -t coffebar -p err
 killall -9 Hyprland
