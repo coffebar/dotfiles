1d920cb714ab7e5f42e3598f9ea6b846bfb98363
Saltaformajo <i8ehkvien@mozmail.com>
2023-11-13 19:08:38 +0200

    fix: don't restart waybar if Hyprland is not running

--- .config/hyprland/start-in-tray.sh
+++ .config/hyprland/start-in-tray.sh
@@ -5,6 +5,7 @@ waybar_loop() {
 	# restart waybar on crash (after suspend mode)
 	WAYBAR_RESTARTS=0
 	while [ $WAYBAR_RESTARTS != 6 ]; do # limited to 5 restars
+		pgrep Hyprland || break # exit if Hyprland is not running
 		waybar
 		let WAYBAR_RESTARTS++
 		notify-send -a "start-in-tray.sh" "Waybar crashed!"
