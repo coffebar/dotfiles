e19a75dbdc7e5fc8928dcce0c2fdb03edc95b512
Saltaformajo <i8ehkvien@mozmail.com>
2024-05-07 11:21:25 +0100

    feat: add audio device switch to monadd.sh

--- .config/hyprland/monadd.sh
+++ .config/hyprland/monadd.sh
@@ -7,3 +7,8 @@ hyprctl monitors | grep $MON2 || exit
 hyprctl dispatch moveworkspacetomonitor 1 $MON2
 hyprctl dispatch moveworkspacetomonitor 3 $MON2
 hyprctl dispatch workspace 1
+
+# switch audio output to speakers
+ALT_SINK=$(pactl list short sinks | tail -2 | head -1 | awk '{print $1}')
+pactl set-default-sink "$ALT_SINK"
+notify-send "Audio Device" "switched from monadd.sh" --expire-time=1500
