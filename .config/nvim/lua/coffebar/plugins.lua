f7cb647e0f66ca362c3186af9d4bc82bf3ff249a
Saltaformajo <i8ehkvien@mozmail.com>
2024-06-07 10:23:41 +0100

    chore: keep barbar.nvim up to date with master

--- .config/nvim/lua/coffebar/plugins.lua
+++ .config/nvim/lua/coffebar/plugins.lua
@@ -237,7 +237,7 @@ return {
     init = function()
       vim.g.barbar_auto_setup = false
     end,
-    version = "^1.0.0", -- optional: only update when a new 1.x version is released
+    -- version = "^1.0.0", -- optional: only update when a new 1.x version is released
   },
   -- git
   {
