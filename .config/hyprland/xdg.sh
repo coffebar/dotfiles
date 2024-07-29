a2dcc7092aa5ffcf0087df21a97db47a150bd6bd
Saltaformajo <i8ehkvien@mozmail.com>
2024-07-29 12:23:06 +0100

    fix(hyprland): remove xdg.sh to start portal from the service

--- .config/hyprland/xdg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sleep 1
-killall -e xdg-desktop-portal-hyprland
-killall -e xdg-desktop-portal-wlr
-killall xdg-desktop-portal
-echo "Starting xdg-desktop-portal-hyprland" >> xdg-desktop-portal-hyprland.log
-/usr/lib/xdg-desktop-portal-hyprland >> xdg-desktop-portal-hyprland.log 2>&1 &
-sleep 2
-/usr/lib/xdg-desktop-portal &
