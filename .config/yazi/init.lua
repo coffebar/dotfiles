b4588d6ece2d3a96d66cbbd3b57f811063676420
coffebar <i8ehkvien@mozmail.com>
2026-04-27 19:18:53 +0100

    feat: add yazi config files

---
+++ .config/yazi/init.lua
@@ -0,0 +1,16 @@
+---@diagnostic disable: undefined-global
+
+-- Add a status line element that shows the user and group
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if not h or ya.target_family() ~= "unix" then
+		return ""
+	end
+
+	return ui.Line({
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		":",
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		" ",
+	})
+end, 500, Status.RIGHT)
