019bde3744df384becf1c4737911f2b48cf13be8
coffebar <i8ehkvien@mozmail.com>
2026-05-16 17:48:25 +0100

    fix(hypr): return missing script

---
+++ .config/hypr/start-in-tray.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# start waybar and programs with tray icons after pause
+
+waybar_loop() {
+	# restart waybar on crash (after suspend mode)
+	WAYBAR_RESTARTS=0
+	while [ $WAYBAR_RESTARTS != 6 ]; do # limited to 5 restars
+		waybar
+		((WAYBAR_RESTARTS++))
+		notify-send -a "start-in-tray.sh" "Waybar crashed!"
+		sleep 1
+	done
+}
+
+# wait for xdg-desktop-portal-hyprland to start
+until pgrep -f 'xdg-desktop-portal-hyprland'; do sleep 2; done
+
+waybar_loop &
+sleep 1 # wait for waybar
+
+if rg -q "Ubuntu" /etc/os-release; then
+	# Ubuntu
+	flatpak run com.slack.Slack --enable-features=UseOzonePlatform --ozone-platform=wayland &
+	~/.local/bin/postman --enable-features=UseOzonePlatform --ozone-platform=wayland &
+	/usr/bin/nordlayer-tray &
+else
+	# Arch Linux
+	XDG_CURRENT_DESKTOP=gnome /usr/bin/Telegram &
+	firefox &
+	hyprctl dispatch moveworkspacetomonitor 2 0
+fi
+
+nm-applet --indicator &
+blueman-applet &
+
+hyprctl dispatch workspace 1
